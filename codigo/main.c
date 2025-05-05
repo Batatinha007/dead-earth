fab853741cbe97d88f876da991e7f320e846b235
batatinha <andriusaugusto@hotmail.com>
2025-05-05 08:21:12 -0300

    ajuste no main

--- codigo/main.c
+++ codigo/main.c
@@ -5,7 +5,7 @@ int main()
 {
 
 printf("Bem vindo ao jogo @@ DEAD EARTH @@n\");
-printf("Esse jogo foi desenvolvido com apredinzagem do Sensei Lucasn\n");
+printf("Esse jogo foi desenvolvido com apredinzagem do Sensei Lucas\n");
 
 }
 
