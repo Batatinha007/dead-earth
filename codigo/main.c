feca771f219817e049b3c2b0753e5cd395fa2c82
batatinha <andriusaugusto@hotmail.com>
2025-05-04 23:21:53 -0300

    codigo ajustado com aspas

--- codigo/main.c
+++ codigo/main.c
@@ -4,5 +4,7 @@ int main()
 
 {
 
-printf(Bem vindo ao jogo @@ DEAD EARTH @@n};
-printf(Esse jogo foi desenvolvido com apredinzagem do Sensei Lucasn); } 
+printf("Bem vindo ao jogo @@ DEAD EARTH @@n\");
+printf("Esse jogo foi desenvolvido com apredinzagem do Sensei Lucasn\"); 
+
+} 
